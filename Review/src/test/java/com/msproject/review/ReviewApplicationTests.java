98b7f25795e3bea19484fe7fbbf1d1eca6b39017
boudidah ilyes <ilyes.boudidah@esprit.tn>
2025-02-11 18:49:08 +0100

    "our MicroServices"

---
+++ Review/src/test/java/com/msproject/review/ReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msproject.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
